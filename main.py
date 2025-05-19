093a43a93601cec2194874f1f8597db7b943ca4c
s30814 <s30814@pjwstk.edu.pl>
2025-05-19 21:21:56 +0200

    Pseudo klasy

---
+++ main.py
@@ -0,0 +1,32 @@
+class Uczen:
+    List < Ocena >
+    List < Obecnosc >
+    Grupa
+
+    def wyswietlSredniaUcznia():
+        srednia = 0
+        ile = 0
+        for i in Oceny:
+            srednia = +i
+            ile + +
+        return srednia / ile
+
+    def czyZagrozony(self):
+        if nieobecnosci > 3:
+            zagrozony
+
+
+class Grupa:
+    List < Uczen >
+
+
+class Ocena:
+    Uczen
+    Opis
+    Data
+
+
+class Obecnosc:
+    Uczen
+    Data
+    Status
